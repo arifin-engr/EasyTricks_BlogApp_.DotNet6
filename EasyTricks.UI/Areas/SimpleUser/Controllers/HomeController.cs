0dd212bf248a4c57faebdc1fb774b0ce030a73ac
ARIFIN <shamsularifin.engr.cse@gmail.com>
2023-01-13 19:26:17 +0600

    primaryDone

--- EasyTricks.UI/Areas/SimpleUser/Controllers/HomeController.cs
+++ EasyTricks.UI/Areas/SimpleUser/Controllers/HomeController.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasyTricks.DAL.Repositories.IRepositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EasyTricks.UI.Areas.SimpleUser.Controllers
 {
     [Area("SimpleUser")]
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork unitOfWork;
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+
+
+            return View(unitOfWork.Article.GetAll().ToList());
         }
     }
 }
